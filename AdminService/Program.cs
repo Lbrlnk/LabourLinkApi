ef606883e84a7478a2d40b51f33bad2faaa7c379
sadinrahman <sadinsadinrahman@gmail.com>
2025-02-05 16:28:56 +0530

    Feat(Muncipality): Add muncipality table and skill managment api

--- AdminService/Program.cs
+++ AdminService/Program.cs
@@ -1,6 +1,10 @@
 
 using AdminService.Data;
+using AdminService.Mapper;
+using AdminService.Repository.MuncipalityRepository;
+using AdminService.Services.MuncipalityService;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 namespace AdminService
 {
@@ -11,8 +15,13 @@ namespace AdminService
             var builder = WebApplication.CreateBuilder(args);
             DotNetEnv.Env.Load();
 
-            // Add configuration
-            builder.Configuration
+			Log.Logger = new LoggerConfiguration()
+	        .WriteTo.Console() 
+	        .WriteTo.File("LogInformation.txt") 
+	        .CreateLogger();
+			builder.Host.UseSerilog();
+			// Add configuration
+			builder.Configuration
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddEnvironmentVariables();
 
@@ -29,8 +38,10 @@ namespace AdminService
 
 
             // Add services to the container.
-
-            builder.Services.AddControllers();
+            builder.Services.AddScoped<IMuncipalityRepository, MuncipalityRepository>();
+            builder.Services.AddScoped<IMuncipalityService, MuncipalityService>();
+			builder.Services.AddAutoMapper(typeof(ProfileMapper));
+			builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
---
+++ AdminService/Repository/MuncipalityRepository/MuncipalityRepository.cs
@@ -0,0 +1,44 @@
+﻿using AdminService.Data;
+using AdminService.Dtos;
+using AdminService.Models;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System.Formats.Asn1;
+
+namespace AdminService.Repository.MuncipalityRepository
+{
+	public class MuncipalityRepository: IMuncipalityRepository
+	{
+		private readonly AdminDbContext _context;
+		
+		public MuncipalityRepository(AdminDbContext context)
+		{
+			_context = context;
+		}
+		public async Task<List<Muncipality>> GetMuncipalitiesAsync()
+		{
+			return await _context.Muncipalities.ToListAsync();
+		}
+		public async Task<Muncipality> AddMuncipalityAsync(Muncipality muncipality)
+		{
+			_context.Muncipalities.Add(muncipality);
+			await _context.SaveChangesAsync();
+			return muncipality;
+		}
+		public async Task<Muncipality> GetMuncipalityByIdAsync(int id)
+		{
+			var mun=await _context.Muncipalities.FirstOrDefaultAsync(x=>x.MunicipalityId==id);
+			return mun; 
+		}
+		public async Task<bool> UpdateMuncipalityAsync(Muncipality muncipality)
+		{
+			_context.Muncipalities.Update(muncipality);
+			return await _context.SaveChangesAsync() > 0;
+		}
+		public async Task<List<Muncipality>> GetMuncipalityByStateAsync(string state)
+		{
+			var muncipalities=await _context.Muncipalities.Where(x=>x.State==state).ToListAsync();
+			return muncipalities;
+		}
+	}
+}
